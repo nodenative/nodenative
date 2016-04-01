a804904726e4971d51adb310943e4749d1ab3b75
Ion Lupascu <ionlupascu@gmail.com>
2016-04-01 07:13:10 +0100

    timer: more tests

--- test/timer_test.cpp
+++ test/timer_test.cpp
@@ -118,3 +118,63 @@ TEST(TimerTests, Repeat3)
     // 100 + 50 + 50
     EXPECT_GE(elapsedTime, 200U);
 }
+
+TEST(TimerTests, Again)
+{
+    std::shared_ptr<Loop> loop = Loop::GetInstanceOrCreateDefault();
+    std::weak_ptr<Loop> loopWeak = loop;
+    int timeouts1 = 0;
+    int timeouts2 = 0;
+    const uint64_t startTime = loop->now();
+    uint64_t elapsedTime1 = 0U;
+    uint64_t elapsedTime2 = 0U;
+
+    {
+        std::shared_ptr<Timer> timer1 = Timer::Create(loop);
+        std::shared_ptr<Timer> timer2 = Timer::Create(loop);
+        std::weak_ptr<Timer> timer1Weak = timer1;
+        std::weak_ptr<Timer> timer2Weak = timer2;
+        EXPECT_EQ(timer1->again().isOK(), false);
+        timer1->onTimeout([&timeouts1, &elapsedTime1, &elapsedTime2, startTime, loopWeak, timer1Weak, timer2Weak](){
+            elapsedTime1 = loopWeak.lock()->now() - startTime;
+
+            EXPECT_EQ(elapsedTime2, 0U);
+            EXPECT_EQ(timer2Weak.lock()->again().isOK(), true);
+
+            if(++timeouts1 >= 3) {
+                timer1Weak.lock()->close();
+            };
+        });
+
+        timer2->onTimeout([&timeouts1, &timeouts2, &elapsedTime1, &elapsedTime2, startTime, loopWeak, timer2Weak](){
+            elapsedTime2 = loopWeak.lock()->now() - startTime;
+
+            EXPECT_EQ(timeouts1, 3);
+            // 20 + 50 + 50
+            EXPECT_GE(elapsedTime1, 120U);
+            // 20 + 50 + 50 + 100
+            EXPECT_GE(elapsedTime2, 220U);
+
+            if(++timeouts2 >= 1) {
+                timer2Weak.lock()->close();
+            };
+        });
+
+        timer1->start(20U, 50U);
+        timer2->start(50U, 100U);
+    }
+
+    EXPECT_EQ(timeouts1, 0);
+    EXPECT_EQ(timeouts2, 0);
+    EXPECT_EQ(elapsedTime1, 0U);
+    EXPECT_EQ(elapsedTime2, 0U);
+
+    loop->run();
+
+    EXPECT_EQ(timeouts1, 3);
+    EXPECT_EQ(timeouts2, 1);
+    // 20 + 50 + 50
+    EXPECT_GE(elapsedTime1, 120U);
+    // 20 + 50 + 50 + 100
+    EXPECT_GE(elapsedTime2, 220U);
+}
